842ac445d76c8eb08c5d0494779031779b3d0d3a
Miklos Eniko <miliczenci@gmail.com>
2021-03-22 14:16:39 +0100

    Final

--- src/components/Hotel.js
+++ src/components/Hotel.js
@@ -1,19 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import LoadingMask from './LoadingMask';
+import React, { useState } from 'react';
+import Subscription from './Subscription';
 
 function Hotel(props) {
     const [det, setDet] = useState(false);
+    const [sub, setSub] = useState(true);
 
     function handleClick() {
+        if (det) setSub(false);
         setDet(!det);
     }
 
     return (
     <div className="hotel">
         <b>{props.data.name}</b><br />
-        {det && `${props.data.city} (${props.data.stars})`}
-        <br />
-        <button onClick={handleClick}>Show {det ? "less" : "more"}</button>
+        <button onClick={handleClick}>Show {det ? "less" : "more"}</button><br /><br />
+        {det ? 
+        <>
+            {props.data.city} ({props.data.stars})<br /><br />
+            {sub && <Subscription hname={props.data.name} show={setSub}/>}
+        </>
+        : null }
     </div>
     );
 }
--- src/components/Subscription.js
+++ src/components/Subscription.js
@@ -2,7 +2,42 @@ import React, { useState, useEffect } from 'react';
 import LoadingMask from './LoadingMask';
 
 function Subscription(props) {
-    return <div>Loading...</div>
+    const [value, setValue] = useState("");
+    const [msg, setMsg] = useState("");
+    const [show, setShow] = useState(false);
+
+    function isValid(str) {
+        return /\w+@\w+\.\w+/.test(str);
+    }
+
+    async function handleSubmit() {
+        setMsg(<LoadingMask />);
+        const rep = await fetch("api/hotels/subscribe", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({email: value, hotel: props.hname})
+        })
+            .then(res => res.json());
+        setMsg(rep.success ? "Subscribed" : "Already subscribed");
+        setTimeout(props.show, 5000, false);
+    }   
+
+    return (
+        <>
+            <button onClick={() => setShow(!show)}>Request more info about {props.hname}</button><br /><br />
+        {msg ? msg :
+            show && 
+            <form onSubmit={handleSubmit}>
+                <label> E-mail:
+                <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
+                </label>
+                <input type="submit" value="Submit" disabled={!isValid(value)} />
+            </form>
+        }
+        </>
+    );
 }
 
 export default Subscription;
